c00b852c45f5d7611306d3a22f2713fb5b2ab395
lightningbat <183808230+lightningbat@users.noreply.github.com>
2026-05-07 20:40:01 +0530

    feat(api): add CORS middleware

--- backend/internal/api/router.go
+++ backend/internal/api/router.go
@@ -34,5 +34,20 @@ func RegisterRoutes(queue *queue.DataQueue, table *dynamo.Table, volatileRdb *re
 		w.WriteHeader(http.StatusOK)
 	})
 
-	return mux
+	return corsHandler(mux)
+}
+
+func corsHandler(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+        if r.Method == http.MethodOptions {
+            w.WriteHeader(http.StatusNoContent)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
 }
